fa3f3cbd60bfb460af6a49fe4433fff5f3b7066f
ManaN Patel <mananpatel320@gmail.com>
2020-05-11 23:44:48 +0530

    package.json updated

--- server.js
+++ server.js
@@ -1,54 +1,53 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const path = require("path");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
-const users = require("./routes/api/users");
-const pr = require("./routes/api/PR");
+const users = require('./routes/api/users');
+const pr = require('./routes/api/PR');
 
 const app = express();
 
 //BodyParser middleware
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false,
+  })
 );
 app.use(bodyParser.json());
 
 //DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //Connect to MongoDB
-mongoose.connect(
-    db,
-    {useNewUrlParser: true,
-    useUnifiedTopology: true}
-)
-.then(() => console.log("MongoDB successfully connected"))
-.catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB successfully connected'))
+  .catch((err) => console.log(err));
 
 //Passport middleware
 app.use(passport.initialize());
 
 //Passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 //Routes
-app.use("/api/users", users);
-app.use("/api/pr", pr);
+app.use('/api/users', users);
+app.use('/api/pr', pr);
 
 //Serve static assets if in production
-if(process.env.NODE_ENV == 'production') {
-    //Set static folder
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV == 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000; //process.env.port for Heroku
 
-app.listen(port, () => console.log(`Server up and running on the port ${port} !`));
+app.listen(port, () =>
+  console.log(`Server up and running on the port ${port}!`)
+);
