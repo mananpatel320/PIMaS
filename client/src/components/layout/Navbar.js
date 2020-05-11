9bb742c527848c5103b953f8e0dabe48bfbff1f7
ManaN Patel <mananpatel320@gmail.com>
2020-05-11 12:47:28 +0530

    minor change in Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
                             }}
                             className="col s5 brand-logo center black-text">
                             <i className="material-icons"></i>
-                            <b>THE PALM</b> INVENTORY MANAGEMENT SYSTEM
+                            THE <b>PALM</b>
                         </Link>
                     </div>
                 </nav>
