143f369f00167d97e9b104378e3b156066b0da05
ManaN Patel <mananpatel320@gmail.com>
2020-05-14 18:35:27 +0530

    added navbar and mobile compatible navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
-import { Provider } from 'react-redux';
-import store from './store';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import PrivateRoute from './components/private-route/PrivateRoute';
 import Dashboard from './components/dashboard/Dashboard';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,119 @@
-import React, { Component } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { logoutUser } from '../../actions/authActions';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className='navbar'>
-        <nav className='z-depth-0'>
-          <div className='nav-wrapper green'>
-            <Link to='/' className='col s5 brand-logo center black-text'>
-              <i className='material-icons'></i>
-              THE <b>PALM</b>
-            </Link>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+const Navbar = ({ auth: { isAuthenticated, loading }, logoutUser }) => {
+  useEffect(() => {
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('.sidenav');
+      M.Sidenav.init(elems, {});
+    });
+  });
+  const authLinks = (
+    <ul class='right hide-on-med-and-down'>
+      <li>
+        <Link to='#!' class='black-text'>
+          PR
+        </Link>
+      </li>
+      <li>
+        <Link to='#!' class='black-text'>
+          PO
+        </Link>
+      </li>
+      <li>
+        <Link onClick={logoutUser} to='#!' class='black-text'>
+          <b>Logout</b>
+        </Link>
+      </li>
+    </ul>
+  );
 
-export default Navbar;
+  const guestLinks = (
+    <ul class='right hide-on-med-and-down'>
+      <li>
+        <Link to='#' class='black-text'>
+          <b>Developers</b>
+        </Link>
+      </li>
+      <li>
+        <Link to='/register' class='black-text'>
+          Register
+        </Link>
+      </li>
+      <li>
+        <Link to='/login' class='black-text'>
+          Login
+        </Link>
+      </li>
+    </ul>
+  );
+
+  const guestLinksnav = (
+    <ul class='sidenav' id='mobile-demo'>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='#!'>
+          <b>Developers</b>
+        </Link>
+      </li>
+    </ul>
+  );
+
+  const authLinksnav = (
+    <ul class='sidenav' id='mobile-demo'>
+      <li>
+        <Link to='#!'>PR</Link>
+      </li>
+      <li>
+        <Link to='#!'>PO</Link>
+      </li>
+      <li>
+        <Link onClick={logoutUser} to='#!'>
+          <b>Logout</b>
+        </Link>
+      </li>
+    </ul>
+  );
+
+  return (
+    <Fragment>
+      <nav className='z-depth-0'>
+        <div className='nav-wrapper green'>
+          <Link to='/' className='brand-logo black-text'>
+            THE <b>PALM</b>
+          </Link>
+          <a href='#!' data-target='mobile-demo' class='sidenav-trigger'>
+            <i class='material-icons black-text'>menu</i>
+          </a>
+          {!loading && (
+            <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+          )}
+        </div>
+        {!loading && (
+          <Fragment>{isAuthenticated ? authLinksnav : guestLinksnav}</Fragment>
+        )}
+      </nav>
+    </Fragment>
+  );
+};
+
+Navbar.prototype = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logoutUser })(Navbar);
